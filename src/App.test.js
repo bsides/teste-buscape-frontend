7653111fae0ed2f4e34f0d5787ffbaec1e024e67
Rafael Pereira <rafaelbsides@gmail.com>
2017-09-05 02:15:42 -0300

    Adding helpers for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import asyncFetch from './helpers/asyncFetch'
+import helpers from './helpers/helpers'
 import fetchMock from 'fetch-mock'
 
 // Simply put, this should render our view
@@ -9,15 +11,27 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div)
 })
 
-// Please notice async/await here
+// Please notice async/await from here on
 it('can fetch data.json', async () => {
   // Anything fetching data will receive this
   fetchMock.get('data.json', { items: [] })
 
-  const response = await fetch('data.json')
+  const response = await asyncFetch('data.json')
   const result = await response.json()
 
   expect(result).toHaveProperty('items')
 })
 
-it(`can handle errors in case fetch doesn't work`, async () => {})
+it(`can handle errors in case fetch doesn't work`, async () => {
+  // Anything >= 400
+  fetchMock.get('http://bad.url', {
+    status: 400,
+    body: JSON.stringify('bad data')
+  })
+
+  const outcome = await helpers.syncify(async () => {
+    return await asyncFetch('http://bad.url')
+  })
+
+  expect(outcome).toThrow()
+})
