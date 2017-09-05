7e21d2af3d15ab216f6dcbf2d0fea489cec722e8
Rafael Pereira <rafaelbsides@gmail.com>
2017-09-05 10:46:33 -0300

    Adding to cart

--- src/Buscape.js
+++ src/Buscape.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react'
 
 import Header from './Header'
-import Products from './Products'
+import Product from './Product'
 
 import './styles/Buscape.css'
 
 class Buscape extends Component {
-  state = { items: [] }
+  state = {
+    items: [],
+    cart: []
+  }
   componentDidMount() {
     this.fetchProducts()
   }
-  fetchProducts() {
-    fetch(
+  addToCart = item => {
+    let found = false
+    let updatedCart = this.state.cart.map(cartItem => {
+      if (cartItem.name === item.name) {
+        found = true
+        cartItem.quantity++
+        return cartItem
+      } else {
+        return cartItem
+      }
+    })
+
+    if (!found) {
+      updatedCart.push({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        quantity: 1
+      })
+    }
+
+    this.setState({
+      cart: updatedCart
+    })
+  }
+  async fetchProducts() {
+    // just to be easier to locate this variable
+    const url =
       'https://raw.githubusercontent.com/bsides/exercicios/master/frontend/resources/data.json'
-    )
-      .then(response => {
-        return response.json()
-      })
-      .then(result => {
-        this.setState({ ...result })
-      })
+
+    const response = await fetch(url)
+    const result = await response.json()
+    this.setState({ ...result })
   }
   render() {
     return (
       <div>
         <Header />
-        <Products data={this.state.items} />
+        <div className="container">
+          {this.state.items.map((item, index) => {
+            return (
+              <Product {...item} addToCart={this.addToCart} key={`p${index}`} />
+            )
+          })}
+        </div>
       </div>
     )
   }
