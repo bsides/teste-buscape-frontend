05d9821347cc41ade7e06f2dcf0038019093ceaa
Rafael Pereira <rafaelbsides@gmail.com>
2017-09-05 02:47:34 -0300

    Since title don't ever change let's use static html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import './index.css'
 import registerServiceWorker from './registerServiceWorker'
-
-document.title = 'Teste Buscapé Frontend'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
